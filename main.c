4806d121917a67a2d8217cd6028884748a717c00
Samuel Torres <arqestorres@gmail.com>
2025-05-12 19:48:09 +0200

    V1.0.0

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-// TODO:
-// CORREGIR ESTETICA Y POR ULTIMO AÑADIR TOPICS
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,24 +14,31 @@ char *line = NULL;
 size_t bufferSize = 0;
 ssize_t characters;
 FILE *fileptr;
-
+char *home;
 int getLastID();
-
+void welcome() {
+  printf("  _____   _                 _               _____   _  __\n");
+  printf(" / ____| | |               | |     /\\      / ____| | |/ /\n");
+  printf("| (___   | |_   _   _    __| |    /  \\    | (___   | ' / \n");
+  printf(" \\___ \\  | __| | | | |  / _ |   / /\\ \\    \\___ \\  |  <  \n");
+  printf(" ____) | | |_  | |_| | | (_| |  / ____ \\   ____) | | . \\ \n");
+  printf("|_____/   \\__|  \\__,_|  \\__,_| /_/    \\_\\ |_____/  |_|\\_\\ \n");
+  printf("**********************************************************"
+         "\n\n\n\n");
+}
 void createFolder(char name[255]) {
   char path[400];
-  const char *home = getenv("HOME");
   if (home != NULL) {
     snprintf(path, sizeof(path), "%s/StudAsk/", home);
     DIR *dir = opendir(path);
-    if (dir) {
-      closedir(dir);
-      printf("Topic alredy created\n");
-    } else {
+    if (!dir) {
 
+      printf("Main StudAsk Folder, not FOUND!\n creating...\n");
       if (mkdir(path, 0777) == 0) {
         printf("Folder created in: %s\n", path);
       }
     }
+    closedir(dir);
     strcat(path, name);
 
     if (mkdir(path, 0777) == 0) {
@@ -44,6 +48,10 @@ void createFolder(char name[255]) {
       printf("Error creating folder, %s", path);
   } else
     printf("Error Obtainining $HOME env variable");
+  // maybe add option to get manually StudAsk folder
+  strcat(path, "/Storage");
+  fileptr = fopen(path, "a");
+  fclose(fileptr);
 }
 
 char *concatString(char *s1, char *s2, char *s3, char *s4) {
@@ -65,11 +73,17 @@ char *concatString(char *s1, char *s2, char *s3, char *s4) {
   return result;
 }
 
-void writeAsk() {
+void writeAsk(char *topicWA) {
+  char path[500];
+  snprintf(path, sizeof(path), "%s/StudAsk/", home);
+  strcat(path, topicWA);
+  strcat(path, "/Storage");
+another:
+  sleep(0);
   char *s1 = NULL, *s2 = NULL, *s3 = NULL, *s4 = NULL;
   size_t fbufferSize = 0;
   ssize_t c1, c2, c3, c4;
-  int id = getLastID();
+  int id = getLastID(path);
   printf("Write the ask:");
   characters = getline(&line, &bufferSize, stdin); // get ask line
 
@@ -94,7 +108,7 @@ void writeAsk() {
   free(s3);
   free(s4);
 
-  fileptr = fopen("Storage", "a");
+  fileptr = fopen(path, "a");
   if (NULL == fileptr) {
     printf("Error opening the file :(");
     exit(0);
@@ -105,9 +119,17 @@ void writeAsk() {
 
   fclose(fileptr);
   free(result);
+  char AnotherQuestion;
+  printf("Desea añadir otra pregunta (N/Y):");
+  scanf(" %c", &AnotherQuestion);
+  getchar();
+  if (AnotherQuestion == 'y' || AnotherQuestion == 'Y') {
+    goto another;
+  }
 }
 
 void shuffle(int arr[], int n) {
+  srand(time(NULL));
   for (int i = n - 1; i > 0; i--) {
     int j = rand() % (i + 1);
     int temp = arr[i];
@@ -123,22 +145,24 @@ void cleanString(char *str) {
   }
 }
 
-void readAsk() {
-
-  int end = getLastID();
+void readAsk(char *topicRA) {
+  int counter = 0;
+  char path[500];
+  snprintf(path, sizeof(path), "%s/StudAsk/", home);
+  strcat(path, topicRA);
+  strcat(path, "/Storage");
+  int end = getLastID(path);
   int index[end];
-
   for (int i = 0; i < end; i++) {
     index[i] = i + 1;
   }
   shuffle(index, end);
-  fileptr = fopen("Storage", "r");
+  fileptr = fopen(path, "r");
   if (NULL == fileptr) {
     printf("Error opening the file :(");
+    sleep(2);
     exit(0);
   }
-  printf("Starting random questions\n");
-  sleep(2);
   for (int I = 0; I < end; I++) {
     rewind(fileptr);
     int lineToRead = index[I];
@@ -156,7 +180,7 @@ void readAsk() {
     // Extract answers with strtok()
     strtok(line, "::!!");
     char *question = strtok(NULL, "::!!");
-    printf("Question: %s\n", question);
+    printf("*Q: %s\n\n", question);
     char *answers[MAX_RESPUESTAS];
     answers[0] = strtok(NULL, "!!::"); // Right correcta
     answers[1] = strtok(NULL, "!!::");
@@ -173,23 +197,19 @@ void readAsk() {
 
     // shuffle answers
     int indices[MAX_RESPUESTAS] = {0, 1, 2, 3};
-    srand(time(NULL));
+
     shuffle(indices, MAX_RESPUESTAS);
 
     // print answers in random positions
-    printf("Please select the right answer\n");
     for (int i = 0; i < MAX_RESPUESTAS; i++) {
-      printf("(%d) %s\n", i + 1, answers[indices[i]]);
+      printf("-(%d): %s\n\n", i + 1, answers[indices[i]]);
     }
 
     // Get users choice
-    char z = 'a';
   Ganswer:
-    z = 'a';
     int userChoice;
     printf("Your Choice: ");
     scanf("%d", &userChoice);
-
     // Validar la respuesta
     if (userChoice >= 1 && userChoice <= MAX_RESPUESTAS) {
       if (strcmp(answers[indices[userChoice - 1]], answers[0]) == 0) {
@@ -199,16 +219,27 @@ void readAsk() {
       }
     } else {
       printf("Invalid option, please try again\n");
+      getchar();
       goto Ganswer;
+    }
+    counter++;
+    if (counter != end) {
+      getchar();
+      printf("Press any key to next question!...");
+      getchar();
+      printf("\n\n");
     }
   }
   fclose(fileptr);
   free(line);
+  getchar();
+  printf("END OF QUESTIONS!\nPress any key to continue!");
+  getchar();
 }
 
 void printDirectories() {
   char path[400];
-  const char *home = getenv("HOME");
+  home = getenv("HOME");
   if (home != NULL) {
     snprintf(path, sizeof(path), "%s/StudAsk/", home);
     DIR *d = opendir(path);
@@ -217,7 +248,7 @@ void printDirectories() {
       while ((dir = readdir(d)) != NULL) {
         // to dont print "." and ".."
         if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
-          printf("%s\n", dir->d_name);
+          printf("---%s\n\n", dir->d_name);
         }
       }
       closedir(d);
@@ -225,9 +256,9 @@ void printDirectories() {
   }
 }
 
-int getLastID() { // This function is to get the last id of the questions
-                  // maybe better with getchar
-  fileptr = fopen("Storage", "r");
+int getLastID(char *path) { // This function is to get the last id of the
+                            // questions maybe better with getchar
+  fileptr = fopen(path, "r");
   while (getline(&line, &bufferSize, fileptr) != -1) {
   }
   fclose(fileptr);
@@ -238,48 +269,76 @@ int getLastID() { // This function is to get the last id of the questions
   int id = atoi(ID);
   return id;
 }
-
+void AskTopic() {
+  char topicVar[256];
+  printf("Plase select one of the following topics:\n\n");
+  printDirectories();
+  printf("Your selection: ");
+  scanf("%s", topicVar);
+  getchar();
+  system("clear");
+  printf("Starting random %s Questions!!!\n", topicVar);
+  sleep(1);
+  system("clear");
+  readAsk(topicVar);
+}
+void WriteTopic() {
+  char topicVar[256];
+  printf("Please select one of the following topics:\n");
+  printDirectories();
+  printf("Your selection: ");
+  scanf("%s", topicVar);
+  getchar();
+  writeAsk(topicVar);
+}
 int main() {
+  home = getenv("HOME");
+  if (home == NULL) {
+    printf("Error Obtainining $HOME ENV Variable");
+  }
 
-  /* inicialize of the code, checking for storage file */
-  // NOT NEEDED IN NEW VERSION
-  if ((fileptr = fopen("Storage", "r")) == NULL) {
-    printf("Error opening main storage file\n");
-    printf("Creating main storage file...\n");
-    fileptr = fopen("Storage", "w");
-    printf("Done!\n");
-    fclose(fileptr);
-  } else
-    printf("Main storage file found\n");
-
+  sleep(1);
 a:
-  printf("please select write or read from storage: \n");
-  printf("Write(1)\tCreate new folder(2)\tRead(3)\n");
-
+  system("clear");
+  welcome();
+  printf("Please Select one of the following options: \n\n");
+  printf(
+      "Ask about a topic(1)\tCreate new Topic(2)\tAdd Questions(3)\tEXIT(0)\n");
+  printf("Your choice: ");
   // Main Menu Code
-  int selector = 0;
+  int selector = -1;
   scanf("%d", &selector);
   getchar();
   switch (selector) {
+  case 0:
+    system("clear");
+    break;
   case 1:
-    writeAsk();
+    system("clear");
+    AskTopic();
+    goto a;
     break;
   case 2:
+    system("clear");
     char folder[255];
     printf("Please enter the name of the folder: ");
-    scanf("%s", &folder);
+    scanf("%s", folder);
     createFolder(folder);
+    goto a;
     break;
   case 3:
-    readAsk();
-    break;
-  case 4:
-    printDirectories();
+    system("clear");
+    WriteTopic();
+    sleep(3);
+    goto a;
     break;
   default:
-    printf("Error in selection\n");
+    printf("Error in selection\nPress any key to continuePress any key to "
+           "continue!!!");
+
+    getchar();
     goto a;
   }
-
+  free(line);
   return 0;
 }
